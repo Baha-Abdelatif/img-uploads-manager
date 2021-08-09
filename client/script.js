24a91fe3c7b3c1d3e3f995875be6149d9b4e5230
Baha-Abdelatif <baha.abdelatif@gmail.com>
2021-08-09 14:58:38 +0200

    Create form and get Inputs content to send them to the backend

--- client/script.js
+++ client/script.js
@@ -1,1 +1,18 @@
-console.log("Well'O Hords !");
+const add_offer_form = document.forms.add_offer_form;
+add_offer_form.addEventListener("submit", add_offer);
+
+const add_picture_form = add_offer_form.add_offer_picture;
+add_picture_form.addEventListener("change", add_picture);
+
+function add_offer(e) {
+  e.preventDefault();
+  const Title = add_offer_form.add_offer_title.value.trim();
+  const Description = add_offer_form.add_offer_description.value.trim();
+  const Category = add_offer_form.add_offer_category.value;
+  console.log("Title: ", Title, "Category: ", Category, "Description: ", Description);
+
+}
+
+function add_picture() {
+  console.log("Image: ", this.files[0])
+}
