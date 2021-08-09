206aa25ff0467d8326f6af033a6f6a3db3c4bfa2
Baha-Abdelatif <baha.abdelatif@gmail.com>
2021-08-09 16:09:37 +0200

    Set up image and fields in backend

--- client/script.js
+++ client/script.js
@@ -1,18 +1,31 @@
+const api_url = "http://localhost:1337";
 const add_offer_form = document.forms.add_offer_form;
 add_offer_form.addEventListener("submit", add_offer);
 
 const add_picture_form = add_offer_form.add_offer_picture;
 add_picture_form.addEventListener("change", add_picture);
+let picture_to_add = null;
 
 function add_offer(e) {
   e.preventDefault();
   const Title = add_offer_form.add_offer_title.value.trim();
   const Description = add_offer_form.add_offer_description.value.trim();
   const Category = add_offer_form.add_offer_category.value;
-  console.log("Title: ", Title, "Category: ", Category, "Description: ", Description);
+  const Picture = picture_to_add;
+  // console.log("Title: ", Title, "Category: ", Category, "Description: ", Description);
+
+  const payload = {
+    Title,
+    Description,
+    Category,
+    Picture
+  }
+
+  // fetch()
 
 }
 
 function add_picture() {
   console.log("Image: ", this.files[0])
+  picture_to_add = this.file[0];
 }
