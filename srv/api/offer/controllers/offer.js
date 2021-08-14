0d983cd95a9908dbcaba76c11f2ba6250868941c
Baha-Abdelatif <baha.abdelatif@gmail.com>
2021-08-14 08:15:37 +0200

    Set up custom controller to return only the published offers

--- srv/api/offer/controllers/offer.js
+++ srv/api/offer/controllers/offer.js
@@ -1,8 +1,24 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+const { sanitizeEntity } = require('strapi-utils');
 
-module.exports = {};
+module.exports = {
+  /**
+   * Retrieve records.
+   *
+   * @return {Array}
+   */
+
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.offer.search(ctx.query);
+    } else {
+      entities = await strapi.services.offer.find(ctx.query);
+    }
+    // Filtre les entités pour retourner celles dont la valeur "isPublished" est à true
+    entities = entities.filter(e => e.isPublished === true)
+
+    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.offer }));
+  },
+};
